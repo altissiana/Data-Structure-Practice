ed9c7bdb927ac1be0326952571c0933902713200
Tissiana Alves <altissiana@gmail.com>
2020-04-24 21:04:00 -0600

    initializing repo

---
+++ DataStructures/Collections/IntLinkedList.cs
@@ -0,0 +1,7 @@
+namespace DataStructures.Collections
+{
+    public class IntLinkedList
+    {
+        
+    }
+}
---
+++ DataStructures/Entities/IntNode.cs
@@ -0,0 +1,9 @@
+namespace DataStructures.Entities
+{
+    public class IntNode
+    {
+        private IntNode value;
+
+        private IntNode nextNode;
+    }
+}
