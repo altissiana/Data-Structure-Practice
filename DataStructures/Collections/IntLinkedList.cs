ffdfc2774c5b719bb849ae1de66e48167bc9982e
Tissiana Alves <altissiana@gmail.com>
2020-04-24 22:25:04 -0600

    added add method

--- DataStructures/Collections/IntLinkedList.cs
+++ DataStructures/Collections/IntLinkedList.cs
@@ -1,7 +1,17 @@
+using DataStructures.Entities;
+
 namespace DataStructures.Collections
 {
     public class IntLinkedList
     {
+        private IntNode node;
+        
+        public void Add(int value)
+        {
+            var node = new IntNode(value, null);
+            this.node = node;
+        }
+
         
     }
 }
