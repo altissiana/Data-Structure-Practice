1b7dac26fba0f532b5fb54c121d79df0085f9225
Tissiana Alves <altissiana@gmail.com>
2020-04-30 15:00:24 -0600

    added tests

---
+++ DataStructureTests/IntLinkedListTests.cs
@@ -0,0 +1,33 @@
+using System;
+using DataStructures.Collections;
+using Xunit;
+
+namespace DataStructureTests
+{
+    public class IntLinkedListTests
+    {
+        public class CountShould
+        {
+            [Fact]
+            public void ReturnZeroWhenListIsEmpty()
+            {
+                IntLinkedList list = new IntLinkedList();
+
+                // Hint: Count should be a C# auto-property
+                Assert.Equal(0, list.Count);
+            }
+
+            [Fact]
+            public void ReturnCountWhenListHasItems()
+            {
+                IntLinkedList list = new IntLinkedList();
+                list.Add(8);
+                list.Add(13);
+                list.Add(21);
+
+                Assert.Equal(3, list.Count);
+            }
+        }
+        
+    }
+}
